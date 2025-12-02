566c0cc749fb7f984e5a0b5f017134890a63b465
Dan Nyanko <dan.nyanko@gmail.com>
2025-12-02 18:38:50 -0500

    feat: introducing red

--- demo-api/app.py
+++ demo-api/app.py
@@ -1,4 +1,4 @@
-import os
+import os, sys
 from flask import Flask
 from prometheus_client import Counter, Histogram, generate_latest, CONTENT_TYPE_LATEST
 import random, time
@@ -28,11 +28,6 @@ LATENCY_THRESHOLD = 1.0  # seconds
 @app.route('/')
 def hello():
     start = time.time()
-
-    # Simulate latency only for red version
-    if VERSION == "red":
-        time.sleep(random.choice([0, 1, 2, 3]))
-
     duration = time.time() - start
 
     # Record latency in histogram
@@ -43,21 +38,33 @@ def hello():
         slow_requests_total.labels(version=VERSION).inc()
 
     # Always increment total requests
-    #   Note: Prometheus labels are always strings. Even numeric values like HTTP 
+    #   NOTE: Prometheus labels are always strings. Even numeric values like HTTP 
     #   status codes should be stored as strings for consistency and compatibility
     #   with PromQL queries.
     http_requests_total.labels(status="200", version=VERSION).inc()
 
     return f"Hello from demo-api {VERSION}!"
 
-@app.route("/foo")
-def foo():
+@app.route("/error")
+def error():
+    # Only red version errors
     if VERSION == "red":
         http_requests_total.labels(status="500", version=VERSION).inc()
         return "Simulated error!", 500
     else:
         http_requests_total.labels(status="200", version=VERSION).inc()
-        return f"Bar {VERSION}", 200
+        return f"No error this time {VERSION}", 200
+    
+@app.route("/crash")
+def crash():
+    # Only red version crashes
+    #   NOTE: Application metrics (like request counts) require explicit instrumentation
+    #    with .inc(). But container lifecycle metrics (like restarts or crash reasons)
+    #    are automatically exported by Kubernetes and scraped by Prometheus. That’s why
+    #    you’ll see crash loops in Grafana without adding .inc() in your code
+    if VERSION == "red":
+        sys.exit(1)  # force container exit
+    return "No crash this time {VERSION}", 200
 
 @app.route('/metrics')
 def metrics():
