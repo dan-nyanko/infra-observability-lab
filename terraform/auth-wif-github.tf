e54723da651213df75720d467cb60482fa75cbf9
Dan Nyanko <dan.nyanko@gmail.com>
2026-01-19 14:02:35 -0500

    fix: move auth resources to project_number and add provider name to allowed_audiences

--- terraform/auth-wif-github.tf
+++ terraform/auth-wif-github.tf
@@ -11,7 +11,7 @@ resource "google_project_service" "iam" {
 
 # Workload Identity Pool
 resource "google_iam_workload_identity_pool" "github_pool" {
-  project                   = var.project_id
+  project                   = var.project_number
   workload_identity_pool_id = "github-pool"
   display_name              = "GitHub Actions Pool"
   description               = "OIDC pool for GitHub Actions"
@@ -19,15 +19,25 @@ resource "google_iam_workload_identity_pool" "github_pool" {
 
 # Workload Identity Provider (GitHub OIDC)
 resource "google_iam_workload_identity_pool_provider" "github_provider" {
-  project                            = var.project_id
-  workload_identity_pool_id          = google_iam_workload_identity_pool.github_pool.workload_identity_pool_id
+  project                            = var.project_number
+  workload_identity_pool_id          = "github-pool"
   workload_identity_pool_provider_id = "github-provider"
   display_name                       = "GitHub Provider"
   description                        = "Trust GitHub OIDC tokens"
-
+  attribute_condition                = "attribute.repository == 'dan-nyanko/infra-observability-lab' && attribute.ref == 'refs/heads/main'"
+  attribute_mapping = {
+    "attribute.actor"            = "assertion.actor"
+    "attribute.ref"              = "assertion.ref"
+    "attribute.repository"       = "assertion.repository"
+    "attribute.repository_owner" = "assertion.repository_owner"
+    "google.subject"             = "assertion.sub"
+  }
   oidc {
-    issuer_uri        = "https://token.actions.githubusercontent.com"
-    allowed_audiences = ["https://github.com/"]
+    allowed_audiences = [
+      "https://github.com/",
+      "//iam.googleapis.com/projects/${var.project_number}/locations/global/workloadIdentityPools/github-pool/providers/github-provider"
+    ]
+    issuer_uri = "https://token.actions.githubusercontent.com"
   }
 
   # Map claims from GitHub’s OIDC token
@@ -55,7 +65,7 @@ resource "google_service_account_iam_binding" "terraform_sa_binding" {
   service_account_id = google_service_account.terraform_sa.name
   role               = "roles/iam.workloadIdentityUser"
 
-  members = [
-    "principalSet://iam.googleapis.com/projects/${var.project_id}/locations/global/workloadIdentityPools/github-pool/attribute.repository/dan-nyanko/infra-observability-lab",
-  ]
+      members = [
+        "principalSet://iam.googleapis.com/projects/${var.project_number}/locations/global/workloadIdentityPools/github-pool/attribute.repository/dan-nyanko/infra-observability-lab",
+      ]
 }
